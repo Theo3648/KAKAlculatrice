02f4bcb2c4701c97b6392d4aa56ef39c13236d10
Théo <Tho@Theo_PCPortable>
2024-10-18 14:34:52 +0200

    Creation projet

---
+++ KAKAlculatrice/src/model/CalculatorModel.java
@@ -0,0 +1,69 @@
+package model;
+import java.util.Stack;
+
+public class CalculatorModel implements CalculatorModelInterface{
+	
+	
+	//Model
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void substract() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void multiply() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void divide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void opposite() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void swap() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ KAKAlculatrice/src/model/CalculatorModelInterface.java
@@ -0,0 +1,27 @@
+package model;
+
+public interface CalculatorModelInterface {
+	
+	//interface
+	
+	public void add();
+	
+	public void substract();
+	
+	public void multiply();
+	
+	public void divide();
+	
+	public void opposite();
+	
+	public void push();
+	
+	public void pop();
+	
+	public void drop();
+	
+	public void swap();
+	
+	public void clear();
+	
+}
